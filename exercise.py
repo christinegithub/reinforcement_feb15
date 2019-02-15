df5ecc10d22fbb62842f467552c0df34d4d0c476
christinegithub <christine13@gmail.com>
2019-02-15 10:32:17 -0500

    exercise complete

---
+++ exercise.py
@@ -0,0 +1,24 @@
+venues = [
+{ 'address': "123 Main Street", 'city': "Toronto", 'wheelchair_accessible': True, 'capacity': 100 },
+{ 'address': "567 Centre Street", 'city': "Toronto", 'wheelchair_accessible': False, 'capacity': 400 },
+{ 'address': "9B Ontario Street", 'city': "Montreal", 'wheelchair_accessible': True, 'capacity': 1000 },
+{ 'address': "56 Road Avenue", 'city': "Ottawa", 'wheelchair_accessible': True, 'capacity': 650 },
+{ 'address': "938 Avenue Way East", 'city': "Toronto", 'wheelchair_accessible': False, 'capacity': 90 },
+{ 'address': "34 Main Street West", 'city': "London", 'wheelchair_accessible': False, 'capacity': 300 },
+{ 'address': "44 Quebec Road", 'city': "Toronto", 'wheelchair_accessible': True, 'capacity': 200 },
+{ 'address': "10 Spruce Avenue Ouest", 'city': "Montreal", 'wheelchair_accessible': False, 'capacity': 525 }
+]
+
+# Find the possible venues that are wheelchair accessible, in Toronto, and can fit at least 150 people.
+# These results should be stored in a list.
+
+new_list = []
+
+for venue in venues:
+    wheelchair = venue['wheelchair_accessible']
+    city = venue['city']
+    capacity = venue['capacity']
+    if wheelchair == True and city == "Toronto" and capacity >= 150:
+        new_list.append(venue['address'])
+
+print(new_list)
